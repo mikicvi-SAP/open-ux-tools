a3ec064272ba1279674803463536f6df3e63b03f
Tobias Queck <tobias.queck@sap.com>
2023-08-21 16:19:17 +0200

    example test and mocking

--- packages/adp-tooling/test/unit/preview/client/control-utils.test.ts
+++ packages/adp-tooling/test/unit/preview/client/control-utils.test.ts
@@ -1,11 +1,30 @@
 import ControlUtils from '../../../../src/preview/client/control-utils';
-
-jest.mock('sap/ui/fl/Utils', () => jest.fn());
+import type ManagedObject from 'sap/ui/base/ManagedObject';
 
 describe('client/control-utils', () => {
-    describe('', () => {
-        test('test something', () => {
-            const result = ControlUtils.getControlAggregationByName({} as any, 'test');
+    describe('getControlAggregationByName', () => {
+        const metadata = {
+            getAllAggregations: jest.fn().mockReturnValue({}),
+            getJSONKeys: jest.fn().mockReturnValue({})
+        };
+        const mockControl = {
+            getMetadata: () => metadata,
+            mockGetSomething: jest.fn().mockReturnValue({})
+        };
+        type controlType = ManagedObject & { __calledJSONKeys?: boolean; [key: string]: any };
+        test('no control provided', () => {
+            const result = ControlUtils.getControlAggregationByName(undefined as unknown as controlType, 'test');
+            expect(result).toHaveLength(0);
+        });
+
+        test('has aggregations', () => {
+            const name = '~name';
+            metadata.getAllAggregations.mockReturnValueOnce({
+                [name]: {
+                    _sGetter: 'mockGetSomething'
+                }
+            });
+            const result = ControlUtils.getControlAggregationByName(mockControl as unknown as controlType, name);
             expect(result).toHaveLength(1);
         });
     });
