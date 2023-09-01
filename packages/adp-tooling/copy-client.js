9f12e884bb60136d4a08f44796d3a11b9edfbed6
Tobias Queck <tobias.queck@sap.com>
2023-09-01 12:46:12 +0200

    use new client

---
+++ packages/adp-tooling/copy-client.js
@@ -0,0 +1,8 @@
+const location = require.resolve('@sap-ux-private/rta-client-extension');
+const fs = require('fs');
+const path = require('path');
+
+const index = fs.readFileSync(location);
+const target = path.join(__dirname, 'dist/preview/client');
+fs.mkdirSync(target);
+fs.writeFileSync(path.join(target, 'index.js'), index);
--- packages/adp-tooling/src/preview/client/index.ts
+++
@@ -1,5 +0,0 @@
-import type RuntimeAuthoring from 'sap/ui/rta/RuntimeAuthoring';
-
-export default (_rta: RuntimeAuthoring) => {
-    // add code to load custom plugins here
-};
